91a32730df5233de26f010f29e690bb266332137
Jason Ding <djhdder@aliyun.com>
2019-09-09 22:18:35 +0800

    testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button>adddd</button>
+    <h1>狗子世界</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
